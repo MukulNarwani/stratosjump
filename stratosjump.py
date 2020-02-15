43a12a1d52c08f54600a26527f6fc10e9701dc1b
MukulNarwani <narwani.m@husky.neu.edu>
2020-02-15 18:05:43 -0500

    Added todo tasks

--- stratosjump.py
+++ stratosjump.py
@@ -6,3 +6,5 @@ file = open("C:\\Users\\narwa\\Downloads\\cartodb-query.geojson","r")
 text = file.read()
 print (text)
 file.close()
+
+#TODO: code equations c(y),b(y),y(t) and v(t), figure out a way to import data.
