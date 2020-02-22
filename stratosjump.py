e249dc5c19bc78a1b4ab015158820350c0bedaaf
MukulNarwani <narwani.m@husky.neu.edu>
2020-02-22 18:35:35 -0500

    coded in y v b c and q

--- stratosjump.py
+++ stratosjump.py
@@ -11,3 +11,17 @@ file.close()
 C=Math.pi() * density * (radius^2) * (velocity^2)/4;
 
 B= 3* Math.pi()* viscosity*2*radius*velocity
+
+q = -(B^2) -4*(-mass*g)*C
+
+
+def v(t):
+	brac = Math.tanh(  (sqrt(-q)/(2*mass))  * (t-tstar))
+	return (sqrt(-q)/(2*C))*(brac - (b/sqrt(-q)))
+
+def y(t):
+	numerator = Math.cosh((t - tstar)*sqrt(-q)/(2*m))
+	denominator = Math.cosh(tstar*sqrt(-q)/(2*m))
+	brac = numerator/denominator
+	return (mass/C)*Math.log(brac) - B*t/(2*C)
+
