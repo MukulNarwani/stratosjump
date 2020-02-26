0f1b0309ff2a39037cee1f9bcfe6da38939431c4
MukulNarwani <narwani.m@husky.neu.edu>
2020-02-26 15:23:43 -0500

    Eulers method approach

--- stratosjump.py
+++ stratosjump.py
@@ -7,21 +7,37 @@ text = file.read()
 print (text)
 file.close()
 
-#TODO: code equations c(y),b(y),y(t),q and v(t), figure out a way to import data.
+#TODO: code equations viscosity density figure deltat out
 C=Math.pi() * density * (radius^2) * (velocity^2)/4;
 
 B= 3* Math.pi()* viscosity*2*radius*velocity
-
+'''
 q = -(B^2) -4*(-mass*g)*C
-
-
+mass = 70
+deltat = 5
+g = 10
 def v(t):
-	brac = Math.tanh(  (sqrt(-q)/(2*mass))  * (t-tstar))
+	brac = Math.tanh(  (sqrt(-q)/(2*mass))  * (deltat))
 	return (sqrt(-q)/(2*C))*(brac - (b/sqrt(-q)))
 
+
+for 
+
+
 def y(t):
-	numerator = Math.cosh((t - tstar)*sqrt(-q)/(2*m))
-	denominator = Math.cosh(tstar*sqrt(-q)/(2*m))
+	numerator = Math.cosh((deltat)*sqrt(-q)/(2*mass))
+	denominator = Math.cosh(deltat*sqrt(-q)/(2*mass))
 	brac = numerator/denominator
 	return (mass/C)*Math.log(brac) - B*t/(2*C)
 
+'''
+
+def y(t):
+	vknot = 0;
+	yknot = 0
+	for i in range(0,t):
+		v = vknot + (gravity - ((Math.pi()*density*(radius^2)*(vknot^4)*0.25)/mass)deltat)
+		y = yknot +(v+vknot)*deltat/2
+		vknot = v
+		yknot = y
+	return y
