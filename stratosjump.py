5ccbdfe400647b0bb24b0cebe95013549205cd9b
MukulNarwani <narwani.m@husky.neu.edu>
2020-02-27 13:13:12 -0500

    shows effect of real gravity vs 9.81 on velocity

--- stratosjump.py
+++ stratosjump.py
@@ -10,10 +10,14 @@ mass = 70
 data = pd.read_csv("pep.csv")
 
 #print(data['Velocity Time (s)'].values.tolist())
-def getgravity(y):
-	h = y + 6378100
-	g = constants.G	*(5.972 * 10**24)/(h**2)
-	return g
+def getgravity(y,real):
+	if(not(real)):
+		 g = 9.81
+		 return g
+	else:
+		h = y + 6378100
+		g = constants.G	*(5.972 * 10**24)/(h**2)
+		return g
 
 
 def getdensity(y):
@@ -30,14 +34,14 @@ def getdensity(y):
 
 	return density
 
-def y(t):
+def y(t,realG=True):
 	deltat = 1;
 	vknot = 0;
 	v =[]
 	y = []
 	yknot = 38969;
 	for i in range(0,int(t/deltat)):
-		v.append(vknot + (getgravity(yknot) - ((math.pi*getdensity(yknot)*(radius**2)*(vknot**2)*0.5)/mass)*deltat))
+		v.append(vknot + (getgravity(yknot,realG) - ((math.pi*getdensity(yknot)*(radius**2)*(vknot**2)*0.5)/mass)*deltat))
 		#Trapezoidal rule to estimate integral 
 		y.append(yknot -((v[len(v)-1]+vknot)*deltat/2))
 		vknot = v[len(v)-1]
@@ -60,5 +64,12 @@ plt.figure(2)
 plt.subplot(111)
 plt.plot(time, y(250)['velocity'])
 plt.plot(data['Velocity Time (s)'],data['Velocity (m/s)'])
+
+plt.figure(3)
+plt.subplot(111)
+plt.plot(time,y(250,False)['velocity'])
+plt.plot(time,y(250)['velocity'])
+
+
 
 plt.show()
